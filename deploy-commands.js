38d1f56c6991abe223023d91f63505e250d03e09
Dominus_Marceau <marceaupatu123@gmail.com>
2023-02-16 16:02:47 +0100

    deploy commands foreach insted of for loop

--- deploy-commands.js
+++ deploy-commands.js
@@ -1,7 +1,8 @@
 const { REST, Routes } = require("discord.js");
 require("dotenv").config();
-const clientId = process.env.clientId;
-const guildId = process.env.guildId;
+
+const { clientId } = process.env;
+const { guildId } = process.env;
 const token = process.env.TOKEN;
 const fs = require("node:fs");
 
@@ -12,10 +13,10 @@ const commandFiles = fs
   .filter((file) => file.endsWith(".js"));
 
 // Grab the SlashCommandBuilder#toJSON() output of each command's data for deployment
-for (const file of commandFiles) {
+commandFiles.forEach((file) => {
   const command = require(`./commands/${file}`);
   commands.push(command.data.toJSON());
-}
+});
 
 // Construct and prepare an instance of the REST module
 const rest = new REST({ version: "10" }).setToken(token);
