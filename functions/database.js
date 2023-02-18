04caa613234db5f1b4712d8d898f8e55853a0621
Dominus_Marceau <marceaupatu123@gmail.com>
2023-02-18 21:33:45 +0100

    Ajout des absences Fixes #5

--- functions/database.js
+++ functions/database.js
@@ -7,7 +7,7 @@ function split(message) {
   let value = message.replaceAll("*", "");
   value = value.replaceAll(" ", "");
   const array = value.split(":").join(",").split("\n").join(",").split(",");
-  let dic = [];
+  const dic = [];
   for (let i = 0; i < array.length; i += 2) {
     dic[array[i]] = array[i + 1];
   }
