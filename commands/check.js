4beab1ef3024597bcdfc1e038fbdf32cd46504e8
Dominus_Marceau <marceaupatu123@gmail.com>
2023-03-21 14:28:03 +0100

    Fix Commande de Check #26

---
+++ commands/check.js
@@ -0,0 +1,60 @@
+const { SlashCommandBuilder } = require("discord.js");
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("check")
+    .setDescription("Permet de check les permissions"),
+  async execute(interaction) {
+    let checkmessage = "";
+    async function checkPermission(
+      permission,
+      channelid = null,
+      clientperm = false,
+      message = null
+    ) {
+      let realmessage = message;
+      if (!message) {
+        if (permission === "ViewChannel") {
+          realmessage = `Voir le channel <#${channelid}>`;
+        } else if (permission === "SendMessages") {
+          realmessage = `Parler dans le channel <#${channelid}>`;
+        } else if (permission === "ManageMessages") {
+          realmessage = `Gérer les messages dans le channel <#${channelid}>`;
+        }
+      }
+      checkmessage = `${checkmessage}\n${realmessage}`;
+      let status = "";
+      try {
+        let perm = false;
+        if (clientperm) {
+          perm = await interaction.guild.members.me.permissions.has(permission);
+        } else {
+          perm = await interaction.guild.channels.cache
+            .get(channelid)
+            .permissionsFor(interaction.client.user)
+            .has(permission);
+        }
+        status = perm ? "✅" : "❌";
+      } catch (e) {
+        console.log(e);
+        status = "❌";
+      } finally {
+        checkmessage = `${checkmessage} ${status}`;
+        await interaction.editReply(checkmessage);
+      }
+    }
+    await interaction.reply({
+      content: "Controle des permissions en cours ⌛",
+      ephemeral: true,
+    });
+    await checkPermission("ViewChannel", process.env.SalonAbsenceLogs);
+    await checkPermission("SendMessages", process.env.SalonAbsenceLogs);
+    await checkPermission("ViewChannel", process.env.SalonBlamelogs);
+    await checkPermission("SendMessages", process.env.SalonBlamelogs);
+    await checkPermission("ViewChannel", process.env.SalonExperienceValide);
+    await checkPermission("SendMessages", process.env.SalonExperienceValide);
+    await checkPermission("ViewChannel", process.env.SalonAbsenceEmbed);
+    await checkPermission("SendMessages", process.env.SalonAbsenceEmbed);
+    await checkPermission("ManageRoles", null, true, "Gérer les rôles");
+  },
+};
